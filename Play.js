0e4352fcae333bc3b5eef1636230f6993dfc1dc2
CTV2 <firewall.brown67@gmail.com>
2023-03-14 21:49:46 -0400

    Added click function to images created function for game stylized button for playing again

--- Play.js
+++ Play.js
@@ -1,7 +1,50 @@
-var choices = ["rock","paper","sheers"];
+var choices = ["Rock","Paper","Scissor"];
 var random = Math.floor(Math.random() * choices.length);
 function play(){
     winner = choices[random];
+    console.log(winner);
+    
 }
-play();
-console.log(winner);
+function vs(){
+    play();
+    if (winner == "Rock"){
+    document.getElementById("win").innerHTML = 'You Lose, Robot Chose Rock!';
+    }
+    else if (winner == "Paper"){
+        document.getElementById("win").innerHTML = 'You Win!, Robot Chose Paper';
+    }
+    else {
+        document.getElementById("win").innerHTML = 'Tie, Robot Chose Scissor';
+    
+    }
+}
+function vss(){
+    play();
+    if (winner == "Rock"){
+        document.getElementById("win").innerHTML = 'You Win!, Robot Chose Rock!';
+        }
+        else if (winner == "Paper"){
+            document.getElementById("win").innerHTML = 'Tie, Robot Chose Paper';
+        }
+        else {
+            document.getElementById("win").innerHTML = 'You Lose, Robot Chose Scissor';
+        
+        }
+}
+function vsss(){
+    play();
+    if (winner == "Rock"){
+        document.getElementById("win").innerHTML = 'Tie!, Robot Chose Rock!';
+        }
+        else if (winner == "Paper"){
+            document.getElementById("win").innerHTML = 'You Lose, Robot Chose Paper';
+        }
+        else {
+            document.getElementById("win").innerHTML = 'You Win!, Robot Chose Scissor';
+        
+        }
+}
+function playagain(){
+    location.reload();
+
+}
